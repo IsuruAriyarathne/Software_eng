8512b4d60802091ffea9950af8b72bb207223ed5
ImajiPietersz <75085245+ImajiPietersz@users.noreply.github.com>
2021-01-16 17:51:31 +0530

    changes to the container folder

--- ui/src/containers/CriminalWeapons/CriminalWeapons.js
+++ ui/src/containers/CriminalWeapons/CriminalWeapons.js
@@ -1,6 +1,9 @@
-import React , {useState} from "react";
+import React , {useState, useEffect, useCallback} from "react";
+import { connect } from 'react-redux';
 
+import {getAllLessons, deleteLessons,updateLessons, saveLessons  } from "../../api/CriminalWeaponsAPI"
 import Table from "../../components/UI/Table/MaterialTable/Table";
+import * as actions from '../../store/actions/index';
 
 const LessonTable = "Criminal Weapons Table";
 
@@ -11,14 +14,74 @@ const tableOptions = {
 
 const CriminalWeapons = props => {
   
-  const [lessons, setLessons] = useState([{id:"null", title:"null", topicid:"null", videoUrl:"null", description:"null",}]);
+  const [lessons, setLessons] = useState([]);
+  useEffect(() => {
+    getAllLessons()
+        .then((response) => {
+          console.log(response.data);
+          if (!response.error) {
+            // (response.data).forEach(user => setUsers(user));
+            setLessons(response.data)
+          }
+        })
+  }, []);
+  const { addAlert } = props; 
+  // const [isLoading, setIsLoading] = useState(true);
+
+ // const [lessons, setLessons] = useState([{id:"null", title:"null", topicid:"null", videoUrl:"null", description:"null",}]);
+
+  const deleteLessons = useCallback(
+    (recoveryID) => {
+      alert("You want to delete " + recoveryID)
+      deleteLessons(recoveryID)
+        .then((response) => {
+            console.log(response);
+            addAlert({
+              message: "Criminal Weapons deletion Successful!",
+            });
+          
+        })
+    },
+    [addAlert]
+  );
+
+  const updateLessons = useCallback(
+    (id,data) => {
+      console.log(data)
+      updateLessons(id,data)
+        .then((response) => {
+            console.log(response);
+            addAlert({
+              message: "Criminal Weapons Updated Successfully!",
+            });
+          
+        })
+    },
+    [addAlert]
+  );
+
+  const saveLessons = useCallback(
+    (data) => {
+      saveLessons(data)
+        .then((response) => {
+          if (!response.error){
+            addAlert({
+              message: "Criminal Weapon Saved Successfully!",
+            });
+          }else{
+            console.log(response.error)
+          }
+        })
+    },
+    [addAlert]
+  );
 
   const tableColumns = [
-    { title: "Id", field: "id" },
-    { title: "Title", field: "title" },
-    { title: "Topic Id", field: "topicid" },
-    { title: "Video URL", field: "videoUrl" },
+    { title: "Id", field: "recoveryID" },
+    { title: "Recovery Date", field: "recoveryDate" },
     { title: "Description", field: "description" },
+    { title: "Station ID", field: "stationID" },
+    
   ];
 
   if (false) {
@@ -31,24 +94,36 @@ const CriminalWeapons = props => {
       tableOptions={tableOptions}
       editable={{
         onRowAdd: newData =>{
-        //   var data=({
-        //     "id": newData.id,
-        //     "title": newData.title,
-        //     "topicId": newData.topic.id,
-        //     "videoUrl": newData.videoUrl,
-        //     "description": newData.description
-        //   })
-        //   saveLesson(data)
+           var data=({
+             "recoveryID": newData.recoveryID,
+             "recoveryDate": newData.recoveryDate,
+             "description": newData.description,
+             "stationID": newData.stationID,  
+           })
+           saveLessons(data)
         },
         onRowUpdate: (newData, oldData) =>{
-          //updateLesson(oldData.id, newData )
+          updateLessons(oldData.recoveryID, newData )
         },
-        onRowDelete: oldData =>{
-          //deleteLesson(oldData.id);
-        },
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            deleteLessons(oldData.recoveryID);
+          }),
+           // {
+        //   deleteUser(oldData.officerID);
+        // },
       }}
     />
   }
 };
 
-export default (CriminalWeapons);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addAlert: alert => dispatch(actions.addAlert(alert))
+  };
+}
+
+export default connect(null, mapDispatchToProps)(CriminalWeapons);
+
+
+//export default (CriminalWeapons);
