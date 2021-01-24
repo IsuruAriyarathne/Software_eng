e17d92b30c5d6eb5ab6105d7ae2b695589a5bfdd
poorna2152 <poorn2152@gmail.com>
2021-01-24 16:32:01 +0530

    update recovery

--- ui/src/api/RecoverAPI.js
+++ ui/src/api/RecoverAPI.js
@@ -6,6 +6,10 @@ export const getAllRecovery = (id) => getRequest(`${BASE_URL}/${id}`);
 
 export const deleteRecovery = (id) => deleteRequest(`${BASE_URL}/${id}`);
 
+export const deleteRecoveryWeapon = (id,weaponModelID) => deleteRequest(`${BASE_URL}/weaponModel/${id}/${weaponModelID}`);
+
+export const deleteRecoveryAmmunition = (id,ammoModelID) => deleteRequest(`${BASE_URL}/ammoModel/${id}/${ammoModelID}`);
+
 export const updateRecovery = (id,data) => putRequest(`${BASE_URL}/${id}`,data);
 
 export const saveRecovery = (data) => postRequest(`${BASE_URL}`,data);
--- ui/src/containers/RecoveredItems/RecoveredItems.js
+++ ui/src/containers/RecoveredItems/RecoveredItems.js
@@ -1,233 +1,239 @@
-import { useEffect, useState ,useCallback} from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { connect } from 'react-redux';
 
-import { useParams } from "react-router-dom";
-// import Spinner from "../../components/UI/Spinner/Spinner";
-import { getRecovery } from '../../api/RecoverAPI';
-// import { changePassword } from "../../api/UsersAPI";
+import { useParams } from 'react-router-dom';
+import { getRecovery, deleteRecoveryAmmunition, deleteRecoveryWeapon, updateRecovery } from '../../api/RecoverAPI';
 import * as actions from '../../store/actions/index';
-// import { SNACKBAR } from "../../components/UI/FHSnackBar/FHSnackBar";
-import { deleteStations, updateStations, saveStations } from "../../api/Stations"
-import {replaceItemInArray, removeItemFromArray, addItemToArray} from "../../shared/utility";
-import Table from "../../components/UI/Table/MaterialTable/Table";
+import { deleteStations, updateStations, saveStations } from '../../api/Stations';
+import { replaceItemInArray, removeItemFromArray, addItemToArray, addItemRemoveDuplicate } from '../../shared/utility';
+import Table from '../../components/UI/Table/MaterialTable/Table';
 
-const ammunitionTable = "Recovered Ammunition Table";
-const weaponTable = "Recovered Weapon Table";
+const ammunitionTable = 'Recovered Ammunition Table';
+const weaponTable = 'Recovered Weapon Table';
 
 const tableOptions = {
-  pageSize: 10,
-  pageSizeOptions: [10, 30, 50]
+	pageSize: 10,
+	pageSizeOptions: [10, 30, 50],
 };
 
-const RecoveryDetail = props => {
-    const { addAlert } = props;
+const RecoveryDetail = (props) => {
+	const { addAlert } = props;
+	const { id } = useParams();
+	const [recoveredammunitions, setrecoveredammunitions] = useState([]);
+	const [recoveredweapons, setrecoveredweapons] = useState([]);
 
-    const { id } = useParams();
-    // const [isLoading, setIsLoading] = useState(true);
-    const [recoveredammunitions, setrecoveredammunitions] = useState([]);
-    const [recoveredweapons, setrecoveredweapons] = useState([]);
+	useEffect(() => {
+		getRecovery(props.stationID, id).then((response) => {
+			if (!response.error) {
+				console.log(response.data);
+				console.log(response.data.RecoveredAmmunitions);
+				setrecoveredammunitions(response.data.RecoveredAmmunitions);
+				setrecoveredweapons(response.data.RecoveredWeapons);
+			}
+		});
+	}, [id]);
 
-    useEffect(() => {
-        getRecovery(props.stationID,id)
-            .then(response => {
-                if (!response.error) {
-                    console.log(response.data);
-                    console.log(response.data.RecoveredAmmunitions);
-                    setrecoveredammunitions(response.data.RecoveredAmmunitions);
-                    setrecoveredweapons(response.data.RecoveredWeapons);
-                }
-            })
-    }, [recoveredammunitions,recoveredweapons,props.stationID, id]);
+	const deleteAmo = useCallback(
+		(oldData) => {
+			return new Promise((resolve, reject) => {
+				deleteRecoveryAmmunition(id, oldData.ammoModelID).then((response) => {
+					console.log(response);
+					if (!response.error) {
+						addAlert({
+							message: 'Ammunition deletion Successful!',
+						});
+						setrecoveredammunitions(
+							removeItemFromArray(recoveredammunitions, 'ammoModelID', oldData.ammoModelID, oldData)
+						);
+						return resolve();
+					}
+					return reject();
+				});
+			});
+		},
+		[addAlert, recoveredammunitions]
+	);
 
-    const deleteAmo = useCallback(
-        (oldData) => {
-          return new Promise((resolve, reject) => {
-            deleteStations(oldData.recoveryID)
-                  .then((response) => {
-                    console.log(response);
-                      if (!response.error) {
-                          addAlert({
-                              message: "Ammunition deletion Successful!",
-                          });
-                          setrecoveredammunitions(removeItemFromArray(recoveredammunitions, 'recoveryID', oldData.recoveryID, oldData))
-                          return resolve();
-                      }
-                      return reject();
-                  })
-          });
-        },
-        [addAlert, recoveredammunitions]
-      );
-    
-      const updateAmo= useCallback(
-        (newData,oldData) => {
-          return new Promise((resolve, reject) => {
-            updateStations(oldData.recoveryID,newData)
-                .then((response) => {
-                    if (!response.error) {
-                        addAlert({
-                            message: "Ammunition Updated Successfully!",
-                        });
-                        setrecoveredammunitions(replaceItemInArray(recoveredammunitions, 'recoveryID', newData, oldData.recoveryID))
-                        return resolve();
-                    }
-                    return reject();
-                })
-          })
-        },
-        [addAlert, recoveredammunitions]
-      );
-    
-      const saveAmo = useCallback(
-        (newStation) => {
-          var data=({
-            "recoveryID": newStation.recoveryID,
-            "ammoModelID": newStation.ammoModelID,
-            "amount": newStation.amount,
-            "name": newStation.name,
-            "description": newStation.description,
-          })
-          return new Promise((resolve, reject) => {
-            saveStations(data)
-                  .then((response) => {
-                      if (!response.error) {
-                          addAlert({
-                              message: "Ammunition Saved Successfully!",
-                          });
-                          setrecoveredammunitions(addItemToArray(recoveredammunitions, response.data))
-                          return resolve();
-                      }
-                      return reject();
-                  })
+	const updateAmo = useCallback(
+		(newData, oldData) => {
+			return new Promise((resolve, reject) => {
+				updateRecovery(oldData.recoveryID, { RecoveredAmmunitions: [newData] }).then((response) => {
+					if (!response.error) {
+						addAlert({
+							message: 'Ammunition Updated Successfully!',
+						});
+						setrecoveredammunitions(
+							replaceItemInArray(recoveredammunitions, 'ammoModelID', newData, oldData.ammoModelID)
+						);
+						return resolve();
+					}
+					return reject();
+				});
+			});
+		},
+		[addAlert, recoveredammunitions]
+	);
+
+	const saveAmo = useCallback(
+		(newStation) => {
+			var data = {
+				RecoveredAmmunitions: [
+					{
+            recoveryID:id,
+						ammoModelID: newStation.ammoModelID,
+						amount: newStation.amount,
+						description: newStation.description,
+					},
+				],
+			};
+			return new Promise((resolve, reject) => {
+				updateRecovery(id, data).then((response) => {
+					if (!response.error) {
+						addAlert({
+							message: 'Ammunition Saved Successfully!',
+						});
+						let arr = addItemRemoveDuplicate(
+							'ammoModelID',
+							recoveredammunitions,
+							response.data.RecoveredAmmunitions
+						);
+						setrecoveredammunitions(addItemToArray(recoveredammunitions, arr[0]));
+						return resolve();
+					}
+					return reject();
+				});
+			});
+		},
+		[addAlert, recoveredammunitions]
+	);
+
+	const deleteWeapon = useCallback(
+		(oldData) => {
+			return new Promise((resolve, reject) => {
+				deleteRecoveryWeapon(id, oldData.weaponModelID).then((response) => {
+					console.log(response);
+					if (!response.error) {
+						addAlert({
+							message: 'Weapon deletion Successful!',
+						});
+						setrecoveredweapons(
+							removeItemFromArray(recoveredweapons, 'weaponModelID', oldData.weaponModelID, oldData)
+						);
+						return resolve();
+					}
+					return reject();
+				});
+			});
+		},
+		[addAlert, recoveredweapons]
+	);
+
+	const updateWeapon = useCallback(
+		(newData, oldData) => {
+			return new Promise((resolve, reject) => {
+				updateRecovery(oldData.recoveryID, newData).then((response) => {
+					if (!response.error) {
+						addAlert({
+							message: 'Weapon Updated Successfully!',
+						});
+						setrecoveredweapons(
+							replaceItemInArray(recoveredweapons, 'weaponModelID', newData, oldData.weaponModelID)
+						);
+						return resolve();
+					}
+					return reject();
+				});
+			});
+		},
+		[addAlert, recoveredweapons]
+	);
+
+	const saveWeapon = useCallback(
+		(newStation) => {
+			var data = {
+				RecoveredWeapons: [
+					{
+            recoveryID:id,
+						weaponModelID: newStation.weaponModelID,
+						amount: newStation.amount,
+						description: newStation.description,
+					},
+				],
+			};
+			return new Promise((resolve, reject) => {
+				updateRecovery(id, data).then((response) => {
+					if (!response.error) {
+						addAlert({
+							message: 'Weapon Saved Successfully!',
             });
-        },
-        [addAlert,recoveredammunitions]
-      );
-      
-      const deleteWeapon = useCallback(
-        (oldData) => {
-          return new Promise((resolve, reject) => {
-            deleteStations(oldData.recoveryID)
-                  .then((response) => {
-                    console.log(response);
-                      if (!response.error) {
-                          addAlert({
-                              message: "Weapon deletion Successful!",
-                          });
-                          setrecoveredweapons(removeItemFromArray(recoveredweapons, 'recoveryID', oldData.recoveryID, oldData))
-                          return resolve();
-                      }
-                      return reject();
-                  })
-          });
-        },
-        [addAlert, recoveredweapons]
-      );
-    
-      const updateWeapon= useCallback(
-        (newData,oldData) => {
-          return new Promise((resolve, reject) => {
-            updateStations(oldData.recoveryID,newData)
-                .then((response) => {
-                    if (!response.error) {
-                        addAlert({
-                            message: "Weapon Updated Successfully!",
-                        });
-                        setrecoveredweapons(replaceItemInArray(recoveredweapons, 'recoveryID', newData, oldData.recoveryID))
-                        return resolve();
-                    }
-                    return reject();
-                })
-          })
-        },
-        [addAlert, recoveredweapons]
-      );
-    
-      const saveWeapon = useCallback(
-        (newStation) => {
-          var data=({
-            "recoveryID": newStation.recoveryID,
-            "ammoModelID": newStation.ammoModelID,
-            "amount": newStation.amount,
-            "name": newStation.name,
-            "description": newStation.description,
-          })
-          return new Promise((resolve, reject) => {
-            saveStations(data)
-                  .then((response) => {
-                      if (!response.error) {
-                          addAlert({
-                              message: "Weapon Saved Successfully!",
-                          });
-                          setrecoveredweapons(addItemToArray(recoveredweapons, data))
-                          return resolve();
-                      }
-                      return reject();
-                  })
-            });
-        },
-        [addAlert,recoveredweapons]
-      );
-    
-      const tableColumnsAmo = [
-        { title: "Recovery ID", field: "recoveryID" },
-        { title: "Ammunition Model ID", field: "ammoModelID" },
-        { title: "Amount", field: "amount" },
-        { title: "Name", field: "name"},
-        { title: "Description", field: "description" },  
-      ];
+            let arr = addItemRemoveDuplicate('weaponModelID',recoveredweapons,response.data.RecoveredWeapons)
+						setrecoveredweapons(addItemToArray(recoveredweapons, arr[0]));
+						return resolve();
+					}
+					return reject();
+				});
+			});
+		},
+		[addAlert, recoveredweapons]
+	);
 
-      const tableColumnsWeapon = [
-        { title: "Recovery ID", field: "recoveryID" },
-        { title: "Weapon Model ID", field: "weaponModelID" },
-        { title: "Amount", field: "amount" },
-        { title: "Name", field: "name"},
-        { title: "Description", field: "description" },  
-      ];
-    
-      if (false) {
-        //return <Spinner />
-      } else {
-        return (
-            <div>
-                <Table
-                    data={recoveredammunitions}
-                    title={ammunitionTable}
-                    columns={tableColumnsAmo}
-                    tableOptions={tableOptions}
-                    editable={{
-                        onRowAdd: newData => saveAmo(newData),
-                        onRowUpdate: (newData, oldData) =>updateAmo(newData, oldData ),
-                        onRowDelete: oldData =>deleteAmo(oldData),
-                    }}
-                />
-                <Table
-                    data={recoveredweapons}
-                    title={weaponTable}
-                    columns={tableColumnsWeapon}
-                    tableOptions={tableOptions}
-                    editable={{
-                        onRowAdd: newData => saveWeapon(newData),
-                        onRowUpdate: (newData, oldData) =>updateWeapon(newData, oldData ),
-                        onRowDelete: oldData =>deleteWeapon(oldData),
-                    }}
-                />
-            </div>
-        )
-      }
-}
+	const tableColumnsAmo = [
+		{ title: 'Model ID', field: 'ammoModelID' },
+		{ title: 'Amount', field: 'amount' },
+		{ title: 'Name', field: 'name', editable:"never" },
+		{ title: 'Description', field: 'description' },
+	];
+
+	const tableColumnsWeapon = [
+		{ title: 'Model ID', field: 'weaponModelID' },
+		{ title: 'Amount', field: 'amount' },
+		{ title: 'Name', field: 'name',editable:"never" },
+		{ title: 'Description', field: 'description' },
+	];
+
+	if (false) {
+		//return <Spinner />
+	} else {
+		return (
+			<div>
+				<Table
+					data={recoveredammunitions}
+					title={ammunitionTable}
+					columns={tableColumnsAmo}
+					tableOptions={tableOptions}
+					editable={{
+						onRowAdd: (newData) => saveAmo(newData),
+						onRowUpdate: (newData, oldData) => updateAmo(newData, oldData),
+						onRowDelete: (oldData) => deleteAmo(oldData),
+					}}
+				/>
+				<Table
+					data={recoveredweapons}
+					title={weaponTable}
+					columns={tableColumnsWeapon}
+					tableOptions={tableOptions}
+					editable={{
+						onRowAdd: (newData) => saveWeapon(newData),
+						onRowUpdate: (newData, oldData) => updateWeapon(newData, oldData),
+						onRowDelete: (oldData) => deleteWeapon(oldData),
+					}}
+				/>
+			</div>
+		);
+	}
+};
 
 const mapStateToProps = (state) => {
-    return {
-        error: state.auth.error,
-        stationID:state.auth.stationID,
-    }
-}
+	return {
+		error: state.auth.error,
+		stationID: state.auth.stationID,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-      addAlert: alert => dispatch(actions.addAlert(alert))
-    };
-}
+	return {
+		addAlert: (alert) => dispatch(actions.addAlert(alert)),
+	};
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(RecoveryDetail);
